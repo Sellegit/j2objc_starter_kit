54430441f3e7332fe209a88c33bdce328b273d8e
simple <simple.continue@gmail.com>
2015-10-21 10:33:44 +0800

    Update cocoapods to the lastest version, test android and ios

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
-source 'https://rubygems.org'
+source 'https://ruby.taobao.org'
 
-gem 'integratej2objc', git:'https://github.com/sellegit/integratej2objc.git'
+gem 'integratej2objc', git:'git@github.com:sellegit/integratej2objc.git'
 gem 'json', '1.8.2'
 gem 'pry'
 gem 'rerun'
--- appname.ios/src/main/java/com/domain/appname/ios/ViewController.java
+++ appname.ios/src/main/java/com/domain/appname/ios/ViewController.java
@@ -1,5 +1,9 @@
 package com.domain.appname.ios;
 
+import apple.coregraphics.CGPoint;
+import apple.coregraphics.CGRect;
+import apple.coregraphics.CGSize;
+import apple.uikit.UIColor;
 import apple.uikit.UIFont;
 import apple.uikit.UILabel;
 import apple.uikit.UIViewController;
@@ -9,9 +13,10 @@ public class ViewController extends UIViewController {
 
     public void viewDidLoad() {
         super.viewDidLoad();
-        UILabel label = new UILabel();
+        UILabel label = new UILabel(CGRect.create(CGPoint.Zero(), CGSize.create(300, 100)));
         label.setText("Hello world!");
-        label.setFont(UIFont.getSystemFont(16));
+        label.setFont(UIFont.getSystemFont(20));
+        this.getView().setBackgroundColor(UIColor.white());
         this.getView().addSubview(label);
     }
 
